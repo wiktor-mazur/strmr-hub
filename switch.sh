06549599c83c01437997fa8753ba12ff65b7c268
Wiktor <me@wiktormazur.pl>
2024-07-31 15:46:33 +0200

    deploy test

--- switch.sh
+++ switch.sh
@@ -1,8 +1,8 @@
 cd ~/domains/taazy.pl
-rm public_nodejs
-unzip versions/$1.zip -d ./versions/$1
-rm -f versions/$1.zip
-ln -s versions/$1 public_nodejs
-cd public_nodejs
+rm -Rf ~/domains/taazy.pl/public_nodejs
+unzip ~/domains/taazy.pl/versions/$1.zip -d ~/domains/taazy.pl/versions/$1
+rm -f ~/domains/taazy.pl/versions/$1.zip
+ln -s ~/domains/taazy.pl/versions/$1 ~/domains/taazy.pl/public_nodejs
+cd ~/domains/taazy.pl/public_nodejs
 npm install --production
 devil www restart taazy.pl
